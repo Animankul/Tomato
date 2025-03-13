92e9b2ba3cc07c69d7b11c108e29f57165821df8
unknown <anishkulkarni182@gmail.com>
2025-03-13 10:56:19 +0530

    Update Vite base path for GitHub Pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/tomato/', // Add this line for GitHub Pages deployment
 })
